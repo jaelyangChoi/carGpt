ba28856f7e80e22568ba8b1b7df8519c0f112166
Jang Hyeon Woong <ds6tvi@gmail.com>
2023-10-16 23:20:48 +0900

    Spring Boot 프로젝트 최초 생성(Spring Boot 2.7.17 / Java 11)

---
+++ backend/car-gpt/src/main/java/com/example/cargpt/CarGptApplication.java
@@ -0,0 +1,13 @@
+package com.example.cargpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarGptApplication.class, args);
+	}
+
+}
