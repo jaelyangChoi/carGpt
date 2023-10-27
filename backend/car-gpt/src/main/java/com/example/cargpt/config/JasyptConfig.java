e36db4561c8899d64593b36dc6a160473920b5e7
Jang Hyeon Woong <ds6tvi@gmail.com>
2023-10-27 10:27:56 +0900

    DB datasource 설정(jasypt 암복호화 적용)

---
+++ backend/car-gpt/src/main/java/com/example/cargpt/config/JasyptConfig.java
@@ -0,0 +1,30 @@
+package com.example.cargpt.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+
+@EnableEncryptableProperties
+@Configuration
+class JasyptConfig {
+ 
+    @Bean("jasyptStringEncryptor")
+    public StringEncryptor stringEncryptor() {
+ 
+        PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+        SimpleStringPBEConfig config = new SimpleStringPBEConfig();
+        
+        config.setPassword("autoeverforever"); // encrypt key
+        
+        config.setAlgorithm("PBEWITHMD5ANDDES");
+        config.setPoolSize("1");
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator");
+        config.setStringOutputType("base64");
+        encryptor.setConfig(config);
+        return encryptor;
+    }
+}
