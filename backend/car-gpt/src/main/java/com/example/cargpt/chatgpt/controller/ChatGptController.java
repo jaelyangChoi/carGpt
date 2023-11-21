dbc7c829b8924e1bd71d2593ca59219f3550b64d
Jang Hyeon Woong <ds6tvi@gmail.com>
2023-11-21 21:18:24 +0900

    고객정보 사용 추천 다듬기

--- backend/car-gpt/src/main/java/com/example/cargpt/chatgpt/controller/ChatGptController.java
+++ backend/car-gpt/src/main/java/com/example/cargpt/chatgpt/controller/ChatGptController.java
@@ -1,8 +1,8 @@
 package com.example.cargpt.chatgpt.controller;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,28 +11,58 @@ import com.example.cargpt.chatgpt.dto.ChatGptRequestDto;
 import com.example.cargpt.chatgpt.dto.UserForRecDto;
 import com.example.cargpt.chatgpt.service.MyChatGptService;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * 2023.10.28 JHW
  */
 @RestController
+@Slf4j
 public class ChatGptController {
 
 	@Autowired
 	MyChatGptService myChatGptService;
 
-	@GetMapping("/StringTest")
-	public String StringTest() {
-		UserForRecDto userForRecDto = new UserForRecDto();
+	/**
+	 * 고객 정보 사용하여 질문하기
+	 * 
+	 * @param map
+	 * 
+		{
+    	"rdnmAdr" : "서울특별시",
+    	"sexCd" : "1",
+    	"csmrTymdNo" : "19840527",
+    	"mariYn" : "1",
+    	"csmrChtSpsn" : "2"
+		}
+	 * @return
+	 */
+	@PostMapping("/question")
+	public String Question(@RequestBody Map<String, String> map) {
+		String rdnmAdr = map.get("rdnmAdr");
+		String csmrTymdNo = map.get("csmrTymdNo");
+		String sexCd = map.get("sexCd");
+		String mariYn = map.get("mariYn");
+		String csmrChtSpsn = map.get("csmrChtSpsn");
 
-		userForRecDto.setRdnmAdr("서울특별시");
-		userForRecDto.setCsmrTymdNo("19960423");
-		userForRecDto.setSexCd("1");
-		userForRecDto.setMariYn("1");
-		userForRecDto.setCsmrChtSpsn("2");
+		log.info(rdnmAdr + " " + csmrTymdNo + " " + sexCd + " " + mariYn + " " + csmrChtSpsn);
+
+		UserForRecDto userForRecDto = new UserForRecDto();
+		userForRecDto.setRdnmAdr(rdnmAdr);
+		userForRecDto.setCsmrTymdNo(csmrTymdNo);
+		userForRecDto.setSexCd(sexCd);
+		userForRecDto.setMariYn(mariYn);
+		userForRecDto.setCsmrChtSpsn(csmrChtSpsn);
 
 		return myChatGptService.makePrompt(userForRecDto);
 	}
 
+	/**
+	 * ChatGPT 질문 던지기
+	 * 
+	 * @param chatGptRequestDto
+	 * @return
+	 */
 	@PostMapping("/sendPrompt")
 	public String test(@RequestBody ChatGptRequestDto chatGptRequestDto) {
 
