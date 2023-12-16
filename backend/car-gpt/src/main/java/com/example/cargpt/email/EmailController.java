1a00121fd870f09fd07473cce10ce62e04d7556e
Jealyang <jaelyangchoi@gmail.com>
2023-12-16 23:36:35 +0900

    이메일에 차량 정보 조회 추가

--- backend/car-gpt/src/main/java/com/example/cargpt/email/EmailController.java
+++ backend/car-gpt/src/main/java/com/example/cargpt/email/EmailController.java
@@ -1,5 +1,7 @@
 package com.example.cargpt.email;
 
+import com.example.cargpt.car.domain.CrmcRecCarRelInfo;
+import com.example.cargpt.car.service.CarService;
 import com.example.cargpt.customer.domain.CustomerInfo;
 import com.example.cargpt.customer.service.CustomerService;
 import lombok.extern.slf4j.Slf4j;
@@ -16,19 +18,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class EmailController {
     private final EmailService emailService;
     private final CustomerService customerService;
+    private final CarService carService;
 
     @Autowired
-    public EmailController(EmailService emailService, CustomerService customerService) {
+    public EmailController(EmailService emailService, CustomerService customerService, CarService carService) {
         this.emailService = emailService;
         this.customerService = customerService;
+        this.carService = carService;
     }
 
     @GetMapping("/simple/{csmrMgmtNo}")
     public EmailVO sendSimpleEmail(@PathVariable Long csmrMgmtNo) {
-        log.info("sendSimpleEmail to : {}", csmrMgmtNo);
-        CustomerInfo customerInfo = customerService.findOne(csmrMgmtNo).get();
-        log.info(customerInfo.getEmlAdr());
-        EmailVO emailVO = makeEmailMessage(customerInfo.getEmlAdr(), "[광고] 차량 추천 안내", "차량 추천 내용");
+        CustomerInfo customerInfo = customerService.findCustomerInfo(csmrMgmtNo).get();
+        EmailVO emailVO = new EmailVO("[테스트] <hyundai-car-recommend@gmail.com>", customerInfo.getEmlAdr(), "차량 추천 제목", "차량 추천 내용");
 
         emailService.sendSimpleEmail(emailVO);
         log.info(emailVO.toString());
@@ -37,17 +39,22 @@ public class EmailController {
 
     @GetMapping("/{csmrMgmtNo}")
     public EmailVO sendEmail(@PathVariable Long csmrMgmtNo) {
-        log.info("sendSimpleEmail to : {}", csmrMgmtNo);
-        CustomerInfo customerInfo = customerService.findOne(csmrMgmtNo).get();
-        log.info(customerInfo.getEmlAdr());
-        EmailVO emailVO = makeEmailMessage(customerInfo.getEmlAdr(), "[광고] 차량 추천 안내", "차량 추천 내용");
+        CustomerInfo customerInfo = customerService.findCustomerInfo(csmrMgmtNo).get();
+        EmailVO emailVO = makeEmailMessage(customerInfo);
 
         emailService.sendEmail(emailVO);
         log.info(emailVO.toString());
         return emailVO;
     }
 
-    public EmailVO makeEmailMessage (String recipient, String subject, String message) {
-        return new EmailVO("Hyundai Car Recommend Service <hyundai-car-recommend@gmail.com>", recipient, subject, message);
+    public EmailVO makeEmailMessage (CustomerInfo customerInfo) {
+        EmailVO emailVO = new EmailVO();
+        CrmcRecCarRelInfo crmcRecCarRelInfo = carService.getCrmcRecCarRelInfo(customerInfo.getCsmrMgmtNo());
+
+        emailVO.setSender("Hyundai Car Recommend Service <hyundai-car-recommend@gmail.com>");
+        emailVO.setRecipient(customerInfo.getEmlAdr());
+//        emailVO.setSubject("[현대자동차] 차량 추천 서비스");customerInfo.get
+//        carService.getCrmcRecCarRelInfo(csmrMgmtNo);
+        return emailVO;
     }
 }
--- backend/car-gpt/src/main/java/com/example/cargpt/email/EmailVO.java
+++ backend/car-gpt/src/main/java/com/example/cargpt/email/EmailVO.java
@@ -1,14 +1,12 @@
 package com.example.cargpt.email;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Getter
 @Setter
-@Builder
 @ToString
+@NoArgsConstructor
+@AllArgsConstructor
 public class EmailVO {
     private String sender;
     private String recipient;
